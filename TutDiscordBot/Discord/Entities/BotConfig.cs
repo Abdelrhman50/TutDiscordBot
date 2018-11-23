b5b8e56175e9c19a02a48cc150f5a5ed24185a1d
Abdelrhman Elsaid <abdelrhmanelsaid681@gmail.com>
2018-11-23 23:15:37 +0200

    Discord Config

---
+++ TutDiscordBot/Discord/Entities/BotConfig.cs
@@ -0,0 +1,11 @@
+﻿using Discord.WebSocket;
+
+namespace TutDiscordBot.Discord.Entities
+{
+    public class BotConfig
+    {
+        public string Token { get; set; }
+
+        public DiscordSocketConfig SocketConfig { get; set; }ً
+    }
+}
---
+++ TutDiscordBot/Discord/SocketConfig.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+
+namespace TutDiscordBot.Discord
+{
+    public static class SocketConfig
+    {
+        public static DiscordSocketConfig GetDefualt()
+        {
+            return new DiscordSocketConfig();
+        }
+
+        public static DiscordSocketConfig Generete()
+        {
+            return new DiscordSocketConfig()
+            {
+
+            };
+        }
+    }
+}
