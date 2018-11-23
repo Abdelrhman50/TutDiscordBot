b691d64b71d55e0e0a55754aafd1eba1f2b5958b
Abdelrhman Elsaid <abdelrhmanelsaid681@gmail.com>
2018-11-23 19:46:22 +0200

    UnityContainer dependencyInjection

---
+++ TutDiscordBot/Unity.cs
@@ -0,0 +1,36 @@
+﻿using TutDiscordBot.DataStorage;
+using TutDiscordBot.DataStorage.Implementations;
+using Unity;
+using Unity.Resolution;
+
+namespace TutDiscordBot
+{
+    public static class Unity
+    {
+        private static UnityContainer _container;
+
+
+        public static UnityContainer Container
+        {
+            get
+            {
+                if(_container == null)
+                    RegisterType();
+                return _container;
+            }
+        }
+
+        public static void RegisterType()
+        {
+            _container= new UnityContainer();
+
+            _container.RegisterType<IDataStorage, InMemoryStorage>();
+        }
+
+        public  static T Resolve<T>()
+        {
+            return (T) Container.Resolve(typeof(T), string.Empty, new CompositeResolverOverride());
+        }
+
+    }
+}
