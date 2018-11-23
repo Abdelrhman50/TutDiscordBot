75439435e9177b923d173fb3d3832bddb273f2e5
Abdelrhman Elsaid <abdelrhmanelsaid681@gmail.com>
2018-11-23 17:14:20 +0200

    first commit

---
+++ TutDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TutDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
